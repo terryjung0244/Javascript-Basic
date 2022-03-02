104793e1faf711a1275f60ce279bad0c4c625a18
Terry Jung <100718033+terryjung0244@users.noreply.github.com>
2022-03-02 09:51:54 -0500

    function

--- function.js
+++ function.js
@@ -35,19 +35,45 @@
 
 // isLeafYear(2022);
 
-function isLeafYear(year) {
-  return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)
-} 
+// function isLeafYear(year) {
+//   return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)
+// } 
 
-console.log(`Is it leaf year in 2022? ${isLeafYear(2022)}`)
+// console.log(`Is it leaf year in 2022? ${isLeafYear(2022)}`)
 
-function sumAll(a, b) { //name of function(parameter, parameter2)
-  let output = 0; //Initial value
-  for (i = a; i <= b; i++) {
-    output += i
+
+// //sum between 1 to 100
+// function sumAll(a, b) { //name of function(parameter, parameter2)
+//   let output = 0; //Initial value
+//   for (i = a; i <= b; i++) {
+//     output += i
+//   }
+//   return output
+// }
+
+// console.log(sumAll(1, 100));
+
+//multiply between 1 to 100
+// function multiplication(a, b) {
+//   let output = 1
+//   for (i = a; i <= b; i++) {
+//     output = output * i
+//   }
+//   return output
+// }
+
+// console.log(multiplication(1, 3))
+
+const min = function (array) {
+  let output = array[0]
+  for (const value of array) {
+    if (output < value) {
+      output = value
+    }
   }
   return output
 }
 
-console.log(sumAll(1, 100));
+console.log(min([22, 33, 44, 55, 66]))
 
+// const max = function ()
