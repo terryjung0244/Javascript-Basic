001d0c6566e8982425dd78d89751e7782adddcf4
Terry Jung <100718033+terryjung0244@users.noreply.github.com>
2022-03-02 11:49:18 -0500

    function

--- function.js
+++ function.js
@@ -64,16 +64,104 @@
 
 // console.log(multiplication(1, 3))
 
-const min = function (array) {
-  let output = array[0]
-  for (const value of array) {
-    if (output < value) {
-      output = value
-    }
-  }
-  return output
-}
+// const min = function (array) {
+//   let output = array[0]
+//   for (const value of array) {
+//     if (output < value) {
+//       output = value
+//     }
+//   }
+//   return output
+// }
 
-console.log(min([22, 33, 44, 55, 66]))
+// // console.log(min([22, 33, 44, 55, 66]))
 
-// const max = function ()
+
+// const max = function (array) {
+//   let output = array[0]
+//   console.log(array[0])
+//   for (const value of array) {  
+    
+//     if (output < value) {
+//       output = value
+      
+//     } 
+//   }
+//   return output
+// }
+// console.log(max([12,523,545,231,753,23,21]))
+
+// console.clear;
+
+// //rest parameter
+// function onClick(...parameters) {
+
+// }
+
+// console.clear;
+
+// function sample(...items) { //make array using by (...items)
+//   console.log(items)
+// }
+// sample(1, 2)
+// sample(1, 2, 3)
+// sample(1, 2, 3, 4)
+
+// console.clear;
+
+// const min = function (...items) {
+//   let output = items[0]
+//   for (const item of items) {
+//     if(output > item) {
+//       output = item
+//     }
+//   }
+//   return output
+// }
+
+// console.log(min(1, 2, 3, 4, 5))
+
+// console.clear;
+
+// const findMax = function (...data) {
+//   let output = data[0]
+//   for (const value of data) {
+//     if (output < value) {
+//       output = value
+//     }
+//   }
+//   return output
+// }
+
+// console.log(findMax(1, 2, 3, 4))
+
+// function sample(a, b, ...c) {
+//   console.log(a, b, c)
+// }
+
+// sample(1, 2)
+// sample(1, 2, 3) // 1, 2, [3]
+// sample(1, 2, 3, 4) // 1, 2, [3, 4]
+
+// //spread operator
+
+// //name of function: onclickfunc
+
+// onclickfunc(...array)
+
+// //example of spread operator 
+
+// function sample(...items) {
+//   console.log(items)
+// }
+
+// const array = [1, 2, 3, 4]
+
+// //without using 'Spread operator'
+// sample(array)
+// //[Array(4)]
+
+// //with using 'Spread operator'
+// sample(...array)
+// //(4) [1, 2, 3, 4]
+
