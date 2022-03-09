73fb50e398365377cc2560bd0442c6d2fd98df1d
Terry Jung <100718033+terryjung0244@users.noreply.github.com>
2022-03-08 22:53:34 -0500

    object

--- object.js
+++ object.js
@@ -19,55 +19,74 @@
 // product.bark()
 // product.sleep()
 
-// // //Two ways of access
-// // console.log(product['name'])
-// console.log(product['type'])
-// console.log(product['ingrient'])
+// // // //Two ways of access
+// // // console.log(product['name'])
+// // console.log(product['type'])
+// // console.log(product['ingrient'])
 
-// console.log(product.name)
-// console.log(product.type)
-// console.log(product.country)
-// console.log(product.array[0])
+// // console.log(product.name)
+// // console.log(product.type)
+// // console.log(product.country)
+// // console.log(product.array[0])
 
-// const javascript = {
-//   name: 'Self Study',
-//   price: '18000',
+// // const javascript = {
+// //   name: 'Self Study',
+// //   price: '18000',
+// //   publisher: 'hanbit'
+// // }
+
+// const pet = {
+//   name: '구름', 
+//   age: 8
+// }
+
+// pet.color = 'brown'
+
+// console.log(pet);
+
+// delete pet.color;
+
+// console.log(pet)
+
+// const book = {
+//   name: 'javascript',
+//   price: 10000,
 //   publisher: 'hanbit'
 // }
 
-const pet = {
-  name: '구름', 
-  age: 8
+// const object = {
+//   en: 'bread',
+//   fr: 'pain',
+//   es: 'pan',
+//   lang: {
+//     ko: '한국어',
+//     en: '영어',
+//     ja: '일본어',
+//     fr: '프랑스어',
+//     es: '스페인어'
+//   },
+//   print: function (lang = 'es') {
+//     console.log(`${object.lang.fr}`)
+//   }
+// }
+
+// object.print('es')
+
+
+//primitives / object 
+
+const a = []
+
+console.log(a)
+
+a.sample = 10
+
+console.log(a)
+
+function b () {
+
 }
 
-pet.color = 'brown'
+b.sample = 10
 
-console.log(pet);
-
-delete pet.color;
-
-console.log(pet)
-
-const book = {
-  name: 'javascript',
-  price: 10000,
-  publisher: 'hanbit'
-}
-
-const object = {
-  en: 'bread',
-  fr: 'pain',
-  es: 'pan',
-  lang: {
-    ko: '한국어',
-    en: '영어',
-    ja: '일본어',
-    fr: '프랑스어',
-    es: '스페인어'
-  },
-  print: function (lang = 'es') {
-    console.log(`${object.lang.fr}`)
-  }
-}
-
-object.print('es')
+console.log(b)
