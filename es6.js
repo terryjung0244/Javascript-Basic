0350665a0233ad8595d05855602af93a795f2871
terryjung0244 <terryjung0244@gmail.com>
2022-03-12 22:42:14 -0500

    es6

--- es6.js
+++ es6.js
@@ -1,11 +1,18 @@
-function checkScope() {
-    let i = 'function scope';
-    if (true) {
-      let i = 'block scope';
-      console.log('Block scope i is: ', i);
-    }
-    console.log('Function scope i is: ', i);
-    return i;
-  }
+// function checkScope() {
+//     let i = 'function scope';
+//     if (true) {
+//       let i = 'block scope';
+//       console.log('Block scope i is: ', i);
+//     }
+//     console.log('Function scope i is: ', i);
+//     return i;
+//   }
 
-  
+// const s = [5, 7, 2];
+// function editInPlace() {
+//   "use strict";
+//   s[0] = 2;
+//   s[1] = 5;
+//   s[2] = 7;
+// }
+// editInPlace();
