7a6afc2c1cf8ba407f4b3d51cd68bc94c9cb561f
terryjung0244 <terryjung0244@gmail.com>
2022-03-13 22:31:50 -0400

    simpleArray

---
+++ dataStructure.js
@@ -0,0 +1,12 @@
+//test for console for live server.
+
+const onclickfunc = () => {
+  console.log("Hello")
+}
+
+onclickfunc()
+
+
+let simpleArray = ['one', 2, 'three', true, 'fall']
+
+console.log(simpleArray.length);
