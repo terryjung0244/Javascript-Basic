93acdfe0de8a81888d64e67c338ac902ea314952
terryjung0244 <terryjung0244@gmail.com>
2022-03-13 23:25:06 -0400

    splice method

--- dataStructure.js
+++ dataStructure.js
@@ -1,88 +1,122 @@
-//test for console for live server.
+// //test for console for live server.
 
-const onclickfunc = () => {
-  console.log("Hello")
-}
+// const onclickfunc = () => {
+//   console.log("Hello")
+// }
 
-onclickfunc()
+// onclickfunc()
 
 
-let simpleArray = ['one', 2, 'three', true, 'fall']
+// let simpleArray = ['one', 2, 'three', true, 'fall']
 
-console.log(simpleArray.length);
+// console.log(simpleArray.length);
 
-const yourArray = ['Monteal', 34, false, 'Jelly', 'future']
-console.log(yourArray);
-console.log(yourArray.length)
+// const yourArray = ['Monteal', 34, false, 'Jelly', 'future']
+// console.log(yourArray);
+// console.log(yourArray.length)
 
-let ourArray = ["a", "b", "c"];
-let ourVariable = ourArray[2];
+// let ourArray = ["a", "b", "c"];
+// let ourVariable = ourArray[2];
 
-console.log(ourVariable);
+// console.log(ourVariable);
 
-console.log(ourArray);
-ourArray[0] = "z"
-console.log(ourArray)
+// console.log(ourArray);
+// ourArray[0] = "z"
+// console.log(ourArray)
 
-console.clear()
+// console.clear()
 
-let myArray = ["a", "b", "c", "d"];
+// let myArray = ["a", "b", "c", "d"];
 
-myArray[1] = "u"
+// myArray[1] = "u"
 
-console.log(myArray);
+// console.log(myArray);
 
-console.clear()
+// console.clear()
 
-//add Items to an Array with push() and unshift()
+// //add Items to an Array with push() and unshift()
 
-//the push() method adds elements to the end of an array.
-//the unshift() method adds elements to the bessing of an array.
+// //the push() method adds elements to the end of an array.
+// //the unshift() method adds elements to the bessing of an array.
 
-let twentyThree = "23";
-let array = ["24", "25"];
+// let twentyThree = "23";
+// let array = ["24", "25"];
 
-array.unshift("22", "23");
-console.log(array);
+// array.unshift("22", "23");
+// console.log(array);
 
-array.push("26", "27");
-console.log(array);
+// array.push("26", "27");
+// console.log(array);
 
-console.clear();
+// console.clear();
 
-function mixedNumbers(arr) {
+// function mixedNumbers(arr) {
  
-  arr.push(7, 'VIII', 9);
-  arr.unshift('I', 2, 'three')
+//   arr.push(7, 'VIII', 9);
+//   arr.unshift('I', 2, 'three')
   
-  return arr;
-}
+//   return arr;
+// }
 
-console.log(mixedNumbers(['IV', 5, 'six']));
+// console.log(mixedNumbers(['IV', 5, 'six']));
 
-console.clear();
+// console.clear();
 
-//Remove items from an Array with pop() and shift()
-//pop(): removes an element from the end of an array.
-//shift(): removes an element from the beginning of an array.
+// //Remove items from an Array with pop() and shift()
+// //pop(): removes an element from the end of an array.
+// //shift(): removes an element from the beginning of an array.
 
-//example
-let greetings = ['whats up?', 'hello', 'see ya'];
-greetings.pop();
-console.log(greetings);
+// //example
+// let greetings = ['whats up?', 'hello', 'see ya'];
+// greetings.pop();
+// console.log(greetings);
 
-greetings.shift();
-console.log(greetings);
+// greetings.shift();
+// console.log(greetings);
 
-let popped = greetings.pop()
-console.log(popped); //hello
+// let popped = greetings.pop()
+// console.log(popped); //hello
+
+// //exercise
+
+// function popShift(arr) {
+//   let popped = arr.pop();
+//   let shifted = arr.shift();
+//   return [shifted, popped];
+// }
+
+// console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+// console.clear();
+
+// //Remove items Using splice()
+// //splice() allows us to remove any number of consecutive elements
+// //splice() can take up to 3 parameters. 
+
+// //example
+// let array0 = ['today', 'was', 'not', 'so', 'great'];
+
+// array0.splice(2, 2);
+
+// console.log(array0);
+
+// console.clear();
+
+// let array1 = ['I', 'am', 'feeling', 'really', 'happy'];
+// let newArray = array1.splice(3, 2);
+
+// console.log(newArray);
 
 //exercise
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+arr.splice(0, 2)
+arr.splice(1, 1)
+arr.splice(1, 1)
+arr.splice(2, 2)
+console.log(arr);
 
-function popShift(arr) {
-  let popped = arr.pop();
-  let shifted = arr.shift();
-  return [shifted, popped];
-}
+const arr1 = [2, 4, 5, 1, 7, 5, 2, 1];
 
-console.log(popShift(['challenge', 'is', 'not', 'complete']));
+arr1.splice(1, 4);
+
+console.log(arr1);
