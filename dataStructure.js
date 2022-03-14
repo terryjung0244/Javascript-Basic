311e70b3bffd776b08de5475c39357281413f701
terryjung0244 <terryjung0244@gmail.com>
2022-03-13 23:03:41 -0400

    push and unshift methods for test

--- dataStructure.js
+++ dataStructure.js
@@ -48,3 +48,16 @@ console.log(array);
 array.push("26", "27");
 console.log(array);
 
+console.clear();
+
+function mixedNumbers(arr) {
+ 
+  arr.push(7, 'VIII', 9);
+  arr.unshift('I', 2, 'three')
+  
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
+
+console.clear();
