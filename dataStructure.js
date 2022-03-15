fd9c1c9d5c6971cf4dd3c05d86a8dcecee27bdea
terryjung0244 <terryjung0244@gmail.com>
2022-03-14 21:48:34 -0400

    excercise push() for looping

--- dataStructure.js
+++ dataStructure.js
@@ -213,3 +213,21 @@ function greaterThanTen(arr) {
 }
 
 greaterThanTen([2, 12, 8, 14, 80, 0, 1]);
+
+console.clear()
+
+function filteredArray(arr, elem) {
+  let newArr = [];
+  
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf(elem) == -1) {
+      console.log(arr[i])
+      newArr.push(arr[i]);
+    }
+  }
+
+  return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
