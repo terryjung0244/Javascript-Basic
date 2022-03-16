4e8f1f42751def3c8d8fc57d9b5699d23a64a625
terryjung0244 <terryjung0244@gmail.com>
2022-03-15 22:49:34 -0400

    excercise nested array

--- dataStructure.js
+++ dataStructure.js
@@ -253,4 +253,17 @@ let nestedArray = [
 
 nestedArray[2][1][0][0][0] = 'deeper still';
 
+console.log(nestedArray[2])
 console.log(nestedArray[2][1][0][0][0]);
+
+//exercise
+let myNestedArray = [
+  
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  ['loop', 'shift', 6, 7, 1000, 'method'],
+  ['concat', false, true, 'spread', 'array',['deep']],
+  ['mutate', 1327.98, 'splice', 'slice', 'push', [['deeper']]],
+  ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth', [[['deepest']]]]
+  
+];
+
