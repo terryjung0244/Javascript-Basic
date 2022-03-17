c7ae35145118de371456eef9ad460f8be4431083
terryjung0244 <terryjung0244@gmail.com>
2022-03-16 23:03:20 -0400

    re object.key()

--- dataStructure.js
+++ dataStructure.js
@@ -471,28 +471,28 @@
 // console.log(countOnline(users));
 
 //exercise
-let users = {
-  Alan: {
-    age: 27,
-    online: false
-  },
-  Jeff: {
-    age: 32,
-    online: true
-  },
-  Sarah: {
-    age: 48,
-    online: false
-  },
-  Ryan: {
-    age: 19,
-    online: true
-  }
-};
+// let users = {
+//   Alan: {
+//     age: 27,
+//     online: false
+//   },
+//   Jeff: {
+//     age: 32,
+//     online: true
+//   },
+//   Sarah: {
+//     age: 48,
+//     online: false
+//   },
+//   Ryan: {
+//     age: 19,
+//     online: true
+//   }
+// };
 
-function getArrayOfUsers(obj) {
+// function getArrayOfUsers(obj) {
   
-  return Object.keys(obj)
+//   return Object.keys(obj)
   
-}
-console.log(getArrayOfUsers(users));
+// }
+// console.log(getArrayOfUsers(users));
