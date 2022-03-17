e7e982bb11dc382fdcc4d42a907941e98c343fef
terryjung0244 <terryjung0244@gmail.com>
2022-03-17 17:54:30 -0400

    Modify an Array Stored

--- dataStructure.js
+++ dataStructure.js
@@ -496,3 +496,37 @@
   
 // }
 // console.log(getArrayOfUsers(users));
+
+//Modify an Array Stored in an Object
+//exercise
+
+let user = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+      username: 'kennethCodesAllDay',
+      joinDate: 'March 26, 2016',
+      organization: 'freeCodeCamp',
+      friends: [
+        'Sam',
+        'Kira',
+        'Tomo'
+      ],
+      location: {
+        city: 'San Francisco',
+        state: 'CA',
+        country: 'USA'
+      }
+    }
+  };
+  
+  function addFriend(userObj, friend) {
+    
+    userObj.data.friends.push(friend)
+    
+    return userObj.data.friends
+  }
+  
+  
+  console.log(addFriend(user, 'Pete'));
+
