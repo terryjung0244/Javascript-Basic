7b1731eb5dc427c5605ec7dbdcbd201f075e97e9
terryjung0244 <terryjung0244@gmail.com>
2022-03-17 22:46:53 -0400

    Factorialize a Number

--- basicAlgorithm.js
+++ basicAlgorithm.js
@@ -7,12 +7,26 @@
 // console.log(convertToF(30));
 
 //Reverse a String
-function reverseString(str) {
-    let splitString = str.split("");
-    let reverseString = splitString.reverse("");
-    let combineString = reverseString.join("");
+// function reverseString(str) {
+//     let splitString = str.split("");
+//     let reverseString = splitString.reverse("");
+//     let combineString = reverseString.join("");
   
-    return combineString
+//     return combineString
+//   }
+  
+//   console.log(reverseString("hello"));
+
+// Factorialize a Number
+function factorialize(num) {
+    if (num < 0)
+      return -1;
+    else if (num == 0)
+      return 1
+    else {
+      return (num * factorialize(num - 1));
+    }
+    
   }
   
-  console.log(reverseString("hello"));
+  factorialize(5);
