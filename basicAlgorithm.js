f380ed44829c012fbc3042ca4ae5c242515b50eb
terryjung0244 <terryjung0244@gmail.com>
2022-03-17 22:09:31 -0400

    Reverse a String

--- basicAlgorithm.js
+++ basicAlgorithm.js
@@ -1,7 +1,18 @@
 //Convert Celsius to Fahrenheit
-function convertToF(celsius) {
-    let fahrenheit = celsius * 9/5 + 32;
-    return fahrenheit;
-}
+// function convertToF(celsius) {
+//     let fahrenheit = celsius * 9/5 + 32;
+//     return fahrenheit;
+// }
 
-convertToF(30);
+// console.log(convertToF(30));
+
+//Reverse a String
+function reverseString(str) {
+    let splitString = str.split("");
+    let reverseString = splitString.reverse("");
+    let combineString = reverseString.join("");
+  
+    return combineString
+  }
+  
+  console.log(reverseString("hello"));
