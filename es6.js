72a488b6f5a5d131bc0f962b472f06680e7dd96a
terryjung0244 <terryjung0244@gmail.com>
2022-03-18 18:27:34 -0400

    exercise

--- es6.js
+++ es6.js
@@ -27,3 +27,24 @@ const person = {
 const greeting = `Hello, my name is ${person.name}`
 console.log(greeting);
 
+//exercise
+
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    
+    const failureItems = [];
+    for (let i = 0; i < arr.length; i++) {
+      failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+    }
+    
+  
+    return failureItems;
+  }
+  
+  const failuresList = makeList(result.failure);
+
+  
