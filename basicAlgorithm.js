b74166a7078b21a5f4a727c9d06b3719dc2c029a
terryjung0244 <terryjung0244@gmail.com>
2022-03-17 23:09:52 -0400

    re

--- basicAlgorithm.js
+++ basicAlgorithm.js
@@ -18,27 +18,27 @@
 //   console.log(reverseString("hello"));
 
 // Factorialize a Number
-function factorialize(num) {
-    if (num < 0)
-      return -1;
-    else if (num == 0)
-      return 1
-    else {
-      return (num * factorialize(num - 1));
-    }
+// function factorialize(num) {
+//     if (num < 0)
+//       return -1;
+//     else if (num == 0)
+//       return 1
+//     else {
+//       return (num * factorialize(num - 1));
+//     }
     
-  }
+//   }
   
-  factorialize(5);
+//   factorialize(5);
 
-  function factorialize(num) {
-      if (num === 0 || num ===1)
-        return 1;
-      for (i = num -1; i >= 1; i--) {
-        return num *= i
-      }
-      return num
-  }
+//   function factorialize(num) {
+//       if (num === 0 || num ===1)
+//         return 1;
+//       for (i = num -1; i >= 1; i--) {
+//         return num *= i
+//       }
+//       return num
+//   }
 
-  factorialize(5);
+//   factorialize(5);
 
